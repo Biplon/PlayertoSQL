0310389dc960c183842a3e33f96043e3f14d230f
Vestri <p.prokop@firenja.com>
2020-07-20 10:26:32 +0200

    dd

--- src/pts/java/event/OnDrop.java
+++ src/pts/java/event/OnDrop.java
@@ -1,5 +1,7 @@
 package pts.java.event;
 
+import com.mysql.jdbc.Buffer;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -11,8 +13,10 @@ public class OnDrop implements Listener
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onDrop(PlayerDropItemEvent ev)
     {
+
         if (PlayertoSql.getInstance().getPlayerManager().disableDrop.contains(ev.getPlayer()))
         {
+            Bukkit.getLogger().warning(ev.getPlayer() + " Ist ein arsch");
             ev.setCancelled(true);
         }
     }
